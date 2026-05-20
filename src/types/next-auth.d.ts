4bd8527428a76dff62f69aba6b232ca8bc95a1f6
Siddique Sakib <dmsakib1122@gmail.com>
2026-05-21 01:07:17 +0600

    types: add next-auth module augmentations

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,15 @@
+import type { DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      username: string;
+    } & DefaultSession["user"];
+  }
+
+  interface JWT {
+    id: string;
+    username: string;
+  }
+}
